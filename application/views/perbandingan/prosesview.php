606419d7935d600a712baad9caec6debcc112a10
Hadi Ubaidillah <ubaycreative@gmail.com>
2018-12-31 20:19:20 +0700

    SPK-PHK

--- application/views/perbandingan/prosesview.php
+++ application/views/perbandingan/prosesview.php
@@ -93,7 +93,7 @@ if($c < 1)
 				<!-- <td><?=ucwords($rKaryawan->status);?></td> -->
 				<td>
 					<?php
-					if ($total >= 2) {
+					if ($total >= 2.5) {
 						echo "Karyawan unggulan";
 					}else{
 						echo "belum unggulan";
